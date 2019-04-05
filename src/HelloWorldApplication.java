6512870483c186d5293bb28f97b9489f39ac7740
Liu <liui3@student.douglascollege.ca>
2019-04-05 10:44:06 -0700

    sophialiu testing 2

--- src/HelloWorldApplication.java
+++ src/HelloWorldApplication.java
@@ -5,6 +5,10 @@ public class HelloWorldApplication {
 		//student name: I-Chia Liu
 		System.out.println("Hello World");
 		System.out.println("Hello World 2");
+		System.out.println("Hello World 3");
+		System.out.println("Hello World 4");
+		System.out.println("Hello World 5");
+		
 		
 
 	}
