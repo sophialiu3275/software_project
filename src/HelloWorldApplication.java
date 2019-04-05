9feb7dea83a7c26ecf4eff19d5b0c291a2808dec
Bola <bolas2@student.douglascollege.ca>
2019-04-05 11:03:28 -0700

    trying resolving the conflict

--- src/HelloWorldApplication.java
+++ src/HelloWorldApplication.java
@@ -4,15 +4,11 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		//student name: I-Chia Liu
 		System.out.println("Hello World");
-<<<<<<< HEAD
-		System.out.println("Simran Kaur Bola");
-		
-	
-=======
-		System.out.println("Hello World 2");
-		
 
->>>>>>> caae6c2432ac11abce161b1219b5118c9acd331f
+	
+
+		System.out.println("Hello World 2");
+
 	}
 
 }
