caae6c2432ac11abce161b1219b5118c9acd331f
Liu <liui3@student.douglascollege.ca>
2019-04-05 10:27:37 -0700

    sophialiu testing

--- src/HelloWorldApplication.java
+++ src/HelloWorldApplication.java
@@ -2,8 +2,11 @@
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
+		//student name: I-Chia Liu
 		System.out.println("Hello World");
-	
+		System.out.println("Hello World 2");
+		
+
 	}
 
 }
