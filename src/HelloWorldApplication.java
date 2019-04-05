59f62b8b172b716fc950550daad7c9a777b9f33f
Liu <liui3@student.douglascollege.ca>
2019-04-05 11:36:44 -0700

    gui app I-Chia Liu

--- src/HelloWorldApplication.java
+++ src/HelloWorldApplication.java
@@ -2,12 +2,14 @@ import javax.swing.*;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
+		//student name:I-Chia Liu
 		JFrame frame = new JFrame("Hello World");
 	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	       frame.setSize(300,300);
 	       JButton button = new JButton("Press");
 	       frame.getContentPane().add(button); // Adds Button to content pane of frame
 	       frame.setVisible(true);
+	       
 	}
 
 }
