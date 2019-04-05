35e3e3698dc6565c339a23742e8963f2bd16bf66
Bola <bolas2@student.douglascollege.ca>
2019-04-05 11:25:12 -0700

    changing

--- src/HelloWorldApplication.java
+++ src/HelloWorldApplication.java
@@ -5,11 +5,7 @@ public class HelloWorldApplication {
 		//student name:I-Chia Liu
 		System.out.println("Hello World");
 
-<<<<<<< HEAD
-	
-=======
-			
->>>>>>> a11edd8eaba747903b49556d70bb3f3efe147ec2
+
 
 		System.out.println("Hello World 2");
 
